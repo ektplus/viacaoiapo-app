3fb53c714ecde757757fd9c62f73b4b2036b181f
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2020-09-18 17:58:31 -0300

    Adicionando SplashScreen e melhorando comportamento no botão fechar

---
+++ lib/home.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_inappbrowser/flutter_inappbrowser.dart';
+
+void main() => runApp(Home());
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Viação Iapó',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: WebViewWebPage(),
+    );
+  }
+}
+
+class WebViewWebPage extends StatefulWidget {
+  @override
+  _WebViewWebPageState createState() => _WebViewWebPageState();
+}
+
+class _WebViewWebPageState extends State<WebViewWebPage> {
+  var url = "https://iapo.crosier.iapo.com.br/app/tur/compra/ini";
+
+  InAppWebViewController webView;
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: _onBack,
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.only(top: 28.0),
+          child: Container(
+            child: Column(
+                children: <Widget>[
+              Expanded(
+                child: Container(
+                  child: InAppWebView(
+                    initialUrl: url,
+                    initialHeaders: {},
+                    initialOptions: {},
+                    onWebViewCreated: (InAppWebViewController controller) {
+                      webView = controller;
+                    },
+                  ),
+                ),
+              )
+            ].where((Object o) => o != null).toList()),
+          ),
+        ),
+      ),
+    ); //Remove null widgets
+  }
+
+  Future<bool> _onBack() async {
+    bool goBack;
+    var value = await webView.canGoBack();
+    if (value) {
+      webView.goBack();
+      return false;
+    } else {
+      await showDialog(
+        context: context,
+        builder: (context) => new AlertDialog(
+          title:
+              new Text('Confirmação ', style: TextStyle(color: Colors.purple)),
+          content: new Text('Sair do aplicativo ?'),
+          actions: <Widget>[
+            new FlatButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+                setState(() {
+                  goBack = false;
+                });
+              },
+              child: new Text('Não'),
+            ),
+            new FlatButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                setState(() {
+                  goBack = true;
+                });
+              },
+              child: new Text('Sim'), // Yes
+            ),
+          ],
+        ),
+      );
+      if (goBack) SystemNavigator.pop();
+      return goBack;
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,62 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_inappbrowser/flutter_inappbrowser.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'home.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Viação Iapó',
+      title: 'Splash Screen',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: WebViewWebPage(),
+      home: MyHomePage(title: 'Splash Screen Flutter'),
     );
   }
 }
 
-class WebViewWebPage extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
   @override
-  _WebViewWebPageState createState() => _WebViewWebPageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _WebViewWebPageState extends State<WebViewWebPage> {
-  Future<bool> _onBack() async {
-    bool goBack;
-    var value = await webView.canGoBack(); // check webview can go back
-    if (value) {
-      webView.goBack(); // perform webview back operation
-      return false;
-    } else {
-      await showDialog(
-        context: context,
-        builder: (context) => new AlertDialog(
-          title:
-              new Text('Confirmação ', style: TextStyle(color: Colors.purple)),
-          // Are you sure?
-          content: new Text('Sair do aplicativo ?'),
-          // Do you want to go back?
-          actions: <Widget>[
-            new FlatButton(
-              onPressed: () {
-                Navigator.of(context).pop(false);
-                setState(() {
-                  goBack = false;
-                });
-              },
-              child: new Text('Não'), // No
-            ),
-            new FlatButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                setState(() {
-                  goBack = true;
-                });
-              },
-              child: new Text('Sim'), // Yes
-            ),
-          ],
-        ),
-      );
-      if (goBack) Navigator.pop(context); // If user press Yes pop the page
-      return goBack;
-    }
-  }
-
-  var url = "https://www.crosier.com.br/viacaoiapo-app";
-
-  // Webview progress
-  double progress = 0;
-  InAppWebViewController webView;
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: _onBack,
-      child: Scaffold(
-          appBar: AppBar(
-            title: Text("Viação Iapó"),
-          ),
-          body: Container(
-              child: Column(
-                  children: <Widget>[
-            (progress != 1.0)
-                ? LinearProgressIndicator(
-                    value: progress,
-                    backgroundColor: Colors.grey[200],
-                    valueColor: AlwaysStoppedAnimation<Color>(Colors.purple))
-                : null, // Should be removed while showing
-            Expanded(
-              child: Container(
-                child: InAppWebView(
-                  initialUrl: url,
-                  initialHeaders: {},
-                  initialOptions: {},
-                  onWebViewCreated: (InAppWebViewController controller) {
-                    webView = controller;
-                  },
-                  onProgressChanged:
-                      (InAppWebViewController controller, int progress) {
-                    setState(() {
-                      this.progress = progress / 100;
-                    });
-                  },
-                ),
-              ),
-            )
-          ].where((Object o) => o != null).toList()))),
-    ); //Remove null widgets
+    return _introScreen();
   }
+}
+
+Widget _introScreen() {
+  return Stack(
+    children: <Widget>[
+      SplashScreen(
+        seconds: 5,
+        gradientBackground: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [
+            Colors.white,
+            Colors.white30
+          ],
+        ),
+        navigateAfterSeconds: Home(),
+        loaderColor: Colors.transparent,
+      ),
+      Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/logo_1920.png"),
+            fit: BoxFit.scaleDown,
+          ),
+        ),
+      ),
+    ],
+  );
 }
